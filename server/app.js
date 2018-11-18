6d025d6640f0fcc13ea59c26ef1febe91bf65701
Iqra Javed <35740252+iqra-javed@users.noreply.github.com>
2018-11-18 14:17:43 -0600

    installed and used graphql & lodash to create schema

--- server/app.js
+++ server/app.js
@@ -1,6 +1,12 @@
 const express = require('express');
+const graphqlHTTP = require("express-graphql"); // middleware
+const schema = require("./schema/schema");
 
 const app = express();
+
+app.use("/graphql", graphqlHTTP({
+    schema // ES6 equivalent of schema: schema 
+}));
 
 app.listen(4000, () => {
     console.log("now listenting for requests on port 4000");
