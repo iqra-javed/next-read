c50f13b92f159a5795b2a2178428e165bcc79ae8
Iqra Javed <35740252+iqra-javed@users.noreply.github.com>
2018-11-18 16:50:24 -0600

    created and connected to instance of mongodb on mlab also installed mongoose and dotenv

--- server/app.js
+++ server/app.js
@@ -1,14 +1,31 @@
 const express = require('express');
-const graphqlHTTP = require("express-graphql"); 
-const schema = require("./schema/schema");
+const graphqlHTTP = require('express-graphql');
+const schema = require('./schema/schema');
+const mongoose = require('mongoose');
+require('dotenv').config();
 
 const app = express();
 
-app.use("/graphql", graphqlHTTP({ // middleware
+// connect to mlab database
+mongoose.connect(
+  `mongodb://${process.env.DB_USER}:${
+    process.env.DB_PASSWORD
+  }@ds211724.mlab.com:11724/next-read`,
+  { useNewUrlParser: true } // useNewUrlParser being used due to deprecation warning in console
+);
+mongoose.connection.once('open', () => {
+  console.log('connected to database');
+});
+
+app.use(
+  '/graphql',
+  graphqlHTTP({
+    // middleware
     schema, // ES6 equivalent of schema: schema
     graphiql: true // graphiql is similar to postman
-}));
+  })
+);
 
 app.listen(4000, () => {
-    console.log("now listenting for requests on port 4000");
-})
+  console.log('now listenting for requests on port 4000');
+});
