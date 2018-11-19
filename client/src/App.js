07f8ab5cb364cf9e552b77d4ff2e9c61e683c755
Iqra Javed <35740252+iqra-javed@users.noreply.github.com>
2018-11-18 20:54:34 -0600

    installed and set up apollo client

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,25 @@
 import React, { Component } from 'react';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo'; /* helps react understand what Apollo is doing by
+ taking data that comes from server and injecting it into the react app */
 
 // components
 import BookList from './components/BookList';
 
+// apollo client setup
+const client = new ApolloClient({
+  uri: 'http://localhost:4000/graphql' // endpoint that we will make requests to
+})
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>Iqra's Reading List</h1>
-        <BookList />
-      </div>
+      <ApolloProvider client={client}>
+        <div className="App">
+          <h1>Iqra's Reading List</h1>
+          <BookList />
+        </div>
+      </ApolloProvider>
     );
   }
 }
