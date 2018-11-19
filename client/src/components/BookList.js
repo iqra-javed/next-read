562a2d3a4e9e47ed825eb03fee84ba4ac7f24faa
Iqra Javed <35740252+iqra-javed@users.noreply.github.com>
2018-11-18 21:19:14 -0600

    retrieved and displayed books' data

--- client/src/components/BookList.js
+++ client/src/components/BookList.js
@@ -1,16 +1,42 @@
 import React, { Component } from 'react';
-import { unlink } from 'fs';
+import { gql } from 'apollo-boost'; // helps parse graphql queries for react
+import { graphql } from 'react-apollo';
+
+// make query
+const getBooksQuery = gql`
+  {
+    books {
+      name
+      id
+    }
+  }
+`;
+
+/* bind query to component so we have access to data 
+that comes back from the query. That data is stored 
+in the component's props */
 
 class BookList extends Component {
+  displayBooks() {
+    let data = this.props.data;
+    if (data.loading) {
+      return <div>Loading books...</div>;
+    } else {
+      return data.books.map(book => {
+        return <li key={book.id}>{book.name}</li>;
+      });
+    }
+  }
   render() {
+    //   console.log(this.props)
     return (
       <div>
-        <ul id="book-list">
-            <li>Book name</li>
+        <ul id='book-list'>
+          {this.displayBooks()}
         </ul>
       </div>
     );
   }
 }
 
-export default BookList;
+export default graphql(getBooksQuery)(BookList);
